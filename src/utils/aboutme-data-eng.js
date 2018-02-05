2bc613839196a7196fad8c75c5ee1a412d9be64a
mawglih <mawglih@gmail.com>
2018-02-05 18:33:27 -0500

    all

--- src/utils/aboutme-data-eng.js
+++ src/utils/aboutme-data-eng.js
@@ -2,16 +2,16 @@ export const AboutMeDataEng = [{
         text: 'Practical life coaching, I combine the methods of coaching and therapy.'
     },
     {
-        text: 'More than 200 clients who changed their destiny after working out their problems together with me.'
+        text: 'More then 200 clients have changed their destiny after working out their problems together with me.'
     },
     {
-        text: 'Professional Education - Institute of Integrative Psychology, Moscow (11/02/2016  - 27/11/2016).'
+        text: 'Professional Education - Institute of Integrative Psychology, Moscow (11/02/2016 - 27/11/2016).'
     },
     {
-        text: 'Master Reiki, RPT Practitioner, Viewer (Remote Vision) - more than 7 years of practice.'
+        text: 'Master Reiki, RPT Practitioner, Viewer (Remote Viewing)) - more than 7 years of practice.'
     },
     {
-        text: 'Instructor ThetaHealing, technology of self-development.'
+        text: 'Instructor of ThethaHealing, technology of self-development.'
     },
     {
         text: 'Proficient in working with OH cards.'
--- src/utils/steps-data-eng.js
+++ src/utils/steps-data-eng.js
@@ -1,12 +1,12 @@
 export const StepsDataEng = [{
         text1: 'A series of paid consultations. Depending on your requirements, you may need from 1 to 10 sessions to fully resolve your problem. One session lasts from 60 to 90 minutes, but if necessary, the time can be increased without additional payment.',
-        text2: 'Sometimes, in order to understand the cause of their situations or cope with their difficulties, one paid session is enough. As a rule, this is a concrete life situation in which you do not know what to do or you lack self-confidence, courage or motivation. For example: ask for a salary increase, find out the relationship with your partner, get through an important interview, competently go on a first date, etc.',
+        text2: 'Sometimes, in order to understand the cause of their situations or cope with their difficulties, one paid session is enough. As a rule, this is a life situation in which you do not know what to do or you lack self-confidence, courage or motivation. For example: ask for a salary increase, find out the relationship with your partner, get through an important interview, competently go on a first date, etc.',
         text3: 'Most of the deep-seated problems associated with relationships, fears, low self-esteem, needs more elaboration with 3-10 sessions.',
         buttonText: 'The cost of 1 session is $50 AUD.'
     },
     {
         text1: 'Free support after the passage of sessions and the onset of improvement. This is a unique proposal.',
-        text2: 'Often after working with a life coach, a person will for some time successfully keep relying on the knowledge gained during the sessions and grow in self-confidence. Over time, emotions can take over and a person can start making mistakes, losing confidence, losing their direction. It is very important here to quickly regain your resources and to your path.',
+        text2: 'Often after working with a life coach, a person will for some time successfully keep relying on the knowledge gained during the sessions and grow in self-confidence. Over time, emotions can take over and a person can start making mistakes, losing confidence, losing their direction. It is very important here to quickly regain your resources and get back to your path.',
         text3: 'My endless support lies in the fact that I am in touch with you on WhatsApp or on Skype. I do not promise ongoing sessions at this stage, but I\'m ready to answer questions and guide you in crisis situations.',
         text4: 'My clients are clients forever. I\'m not leaving you.'
     },
@@ -19,10 +19,10 @@ export const StepsDataEng = [{
     {
         heading1: 'WORK WITH ME CONSISTS OF 3 STAGES.',
         heading2: '2 OF THESE STAGES - FREE OF CHARGE FOR YOU.',
-        heading3: '1 stage',
-        heading4: ' Free consultation. It is necessary for us to understand if we feel comfortable with each other and you be able to open up with me and start to see progress at this stage.',
-        heading5: '2 stage',
-        heading6: '3 stage',
+        heading3: 'Stage 1',
+        heading4: 'Free consultation . It is necessary for us to understand if we feel comfortable with each other and you are able to open up with me and start to see progress at this stage.',
+        heading5: 'Stage 2',
+        heading6: 'Stage 3',
         heading7: 'In working with each client, I use those methods that are most effective in solving the specific problems.'
     },
     {
